10aded48a64e051b33ca64c06b6014c939c4385e
roharon <roharon@hufs.ac.kr>
2018-05-08 19:20:43 +0900

    태그 간략하게 수정

--- 5주차/naver_crawl.py
+++ 5주차/naver_crawl.py
@@ -33,7 +33,7 @@ def crawl():
     #real = soup.find("li", class_='ah_item')
     # li 태그 중 class이름이 'ah_item' 인 것만을 골라줍니다.
 
-    real = soup.find_all('li', class_='ah_item')
+    real = soup.find_all('span', class_='ah_k')
     # class는 파이썬의 예약어이기 때문에 class_ 라고 써줘야 beautifulSoup라는 라이브러리에서 인식이 가능하다.
     # 단순히 class 라고 써버리면 파이썬의 클래스로 알아들어 오류가 발생한다
 
@@ -45,28 +45,14 @@ def crawl():
 
         all_list.append(i.text)
         # i의 텍스트가 all_list라는 리스트 변수에 하나씩 저장된다.
-        # -> 여기까지 하면 ['\n\n1\n붐붐파워\n\n', '\n\n4\n최은주\n\n', 와 같이 뜰텐데
-        # replace를 이용해 \n을 지우면 된다. (리스트에서 replace는 안되므로 for문을 써서 원소 하나마다 replace를 하면된다.)
-
-    for index, word in enumerate(all_list):
-        word=word.replace('\n','')
-        # '\n\n1\n붐붐파워\n\n' 의 \n을 모두 제거한다.
-        all_list[index] = word
+        # -> 여기까지 하면 [<span class="ah_k">현도훈</span>, <span class="ah_k">황현희</span>, 와 같이 뜰텐데
+        # i.text 와 같이 text를 쓰면 사람이 바로 볼 수 있는 글자만 나타내준다.
 
     print(all_list)
-
-    """
-    출력하면 원소마다 숫자(검색순위)가 붙게되는데
-    인덱싱을 통해 
-    all_list[index] = word[1:] 과 같이 처리해줘도 됩니다. (숫자가 문자열의 맨앞에 붙으니 이 부분을 자르는 방식)
-    
-    단지 제가 자주 쓰는 방법이며 정답은 없습니다! (모범답안도 아니에요)
-    """
 
 
 if __name__ == "__main__":
     crawl()
-    crawl_practice()
 
 """
 if __name__== "__main__":
