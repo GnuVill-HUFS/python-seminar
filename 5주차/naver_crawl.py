49b3c2eb49968554430504e8e429e0332d95d072
roharon <roharon@hufs.ac.kr>
2018-05-10 11:32:12 +0900

    5주차 커밋

--- 5주차/naver_crawl.py
+++ 5주차/naver_crawl.py
@@ -25,7 +25,8 @@ def crawl():
     # req.text 형태로 해주면 html코드로 나타내어 줍니다.
 
     soup = BeautifulSoup(html,'lxml')
-    # lxml은 문서코드를 읽어주는 도구 중 하나입니다.
+    # lxml은 문서코드를 읽어주는 도구(parser) 중 하나입니다.
+    # 이 것을 parsing 이라 합니다.
 
     #real = soup.find("div", class_='ah_list PM_CL_realtimeKeyword_list_base')
     # div태그 중 class이름이 'ah_list PM_CL_realtimeKeyword_list_base' 인 것만을 골라줍니다.
@@ -37,19 +38,20 @@ def crawl():
     # class는 파이썬의 예약어이기 때문에 class_ 라고 써줘야 beautifulSoup라는 라이브러리에서 인식이 가능하다.
     # 단순히 class 라고 써버리면 파이썬의 클래스로 알아들어 오류가 발생한다
 
+    print(real)
     all_list = []
     for i in real:
-
         #print(i.text)
         # 변수.text 는 태그에서 사람들이 볼 수 있는 텍스트만을 출력해줍니다.
-
         all_list.append(i.text)
         # i의 텍스트가 all_list라는 리스트 변수에 하나씩 저장된다.
         # -> 여기까지 하면 [<span class="ah_k">현도훈</span>, <span class="ah_k">황현희</span>, 와 같이 뜰텐데
         # i.text 와 같이 text를 쓰면 사람이 바로 볼 수 있는 글자만 나타내준다.
 
-    print(all_list)
-
+    for number, info in enumerate(all_list):
+        if number >= 20:
+            break
+        print(info)
 
 if __name__ == "__main__":
     crawl()
